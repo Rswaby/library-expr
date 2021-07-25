e619132414be020000677a1eb0019858adc4e6cc
Rohan Swaby <rohan_swaby51@yahoo.com>
2021-07-25 15:25:24 -0400

    display all books

--- routes/books.js
+++ routes/books.js
@@ -16,9 +16,9 @@ const routeHandler = callback =>{
 /* GET books listing. */
 router.get('/',routeHandler( async (req, res) => {
   console.log('fetching books...');
-  const allBooks = await Book.findAll();
-  console.log(`returned ${allBooks.length} books from db`);
-  res.json(allBooks);
+  const books = await Book.findAll();
+  console.log(`returned ${books.length} books from db`);
+  res.render('index', {books});
 }));
 
 router.get('/new',(req, res) => {
@@ -26,20 +26,37 @@ router.get('/new',(req, res) => {
 });
 
 router.post('/new',routeHandler( async (req, res) => {
-  res.json({id:'lkjlkjadf@3',op:'Create'});
+  console.log('creating new book...',req.body);
+  const newBook = await Book.create(req.body);
+  res.json(newBook);
 }));
 
 router.get('/:id',routeHandler( async (req, res) =>{
-  res.json({id:req.params.id,op:'Get'});
+  const id = req.params.id;
+  console.log(`retrieving book with id: ${id}`)
+  const oldbook = await Book.findByPk(req.params.id);
+  //todo: maybe change this to internal server error?
+  if (!book){
+    res.redirect(`/page-not-found/${id}`);
+  }
+  res.json(book);
 }));
 /**
  * update book with id = req.params.id
  */
-router.post('/:id',routeHandler( (req, res) =>{
-  res.json({id:req.params.id,op:'Update'});
+router.post('/:id',routeHandler( async (req, res) =>{
+  const id = req.params.id;
+  const oldbook = await Book.findByPk(id);
+  const newbook = await oldbook.update(req.body);
+  res.json(newbook);
+  // res.redirect(`/books/${id}`);
 }));
 
 router.post('/:id/delete',routeHandler( async (req,res)=>{
-  res.json({id:req.params.id,op:'Delete'});
+  const id = req.params.id;
+  console.log(`deleting record with id ${id}...`)
+  const oldbook = await Book.findByPk(id);
+  const deleted = await oldbook.destroy();
+  res.json(deleted); 
 }));
 module.exports = router;
